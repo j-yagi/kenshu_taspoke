dc71b79b22fbeaec4ed47697b1cc29b2e75d0d04
j_yagi <yagi@se-sendai.co.jp>
2022-12-23 17:23:58 +0900

    ブランチ作り直し

---
+++ public_html/account/index.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * マイアカウント画面
+ * 
+ * @since 1.0.0
+ */
+
+require_once '../../bootstrap.php';
+require_once ROOT_DIR . '/app/Controllers/UserController.php';
+
+// if(!empty($_POST['name'])){
+//         $ctrl = new UserController();
+//         $ctrl->update($_POST['name'],$_POST['email'],$_POST['password']);
+// }
+
+$ctrl = new UserController();
+extract($ctrl->edit());
+$title = 'プロジェクト' . (Request::getParam('id') ? '更新' : '登録');
+
+?>
+
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+    <?php include HTML_DIR . '/components/head.php'?>
+</head>
+
+<body class="text-body bg-light">
+    <div class="d-flex">
+
+        <?php include HTML_DIR . '/components/sidenav.php' ?>
+
+        <div class="container p-3">
+            <h2 class="fs-4 mb-4"><?= $title ?></h2>
+            
+            <form action="" method="post">
+                <label for="name">*アカウント名</label>
+                    <input type="text" id="name" name="name" require>
+                <label for="email">*メールアドレス</label>
+                    <input type="email" id="email" name="email" require>
+                <label for="password">*パスワード</label>
+                    <input type="password" id="password" name="password" require>
+                    <br>
+
+                    <div class="mb-3">
+                    <label for="name" class="form-label">
+                        <span class="text-danger">*</span> アカウント名
+                    </label>
+                    <?php if (!isset($errors['name'])) : ?>
+                        <input type="text" name="name" class="form-control" id="name" value="<?= h($old['name'] ?? $project->name) ?>">
+                    <?php else : ?>
+                        <input type="text" name="name" class="form-control is-invalid" id="name" value="<?= h($old['name'] ?? $project->name) ?>">
+                        <div class="invalid-feedback">
+                            <?php foreach ($errors['name'] as $message) : ?>
+                                <div><?= $message ?></div>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                    <div class="mb-3">
+                    <label for="name" class="form-label">
+                        <span class="text-danger">*</span> アカウント名
+                    </label>
+                    <?php if (!isset($errors['name'])) : ?>
+                        <input type="text" name="name" class="form-control" id="name" value="<?= h($old['name'] ?? $project->name) ?>">
+                    <?php else : ?>
+                        <input type="text" name="name" class="form-control is-invalid" id="name" value="<?= h($old['name'] ?? $project->name) ?>">
+                        <div class="invalid-feedback">
+                            <?php foreach ($errors['name'] as $message) : ?>
+                                <div><?= $message ?></div>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                    <div class="mb-3">
+                    <label for="email" class="form-label">
+                        <span class="text-danger">*</span> メールアドレス
+                    </label>
+                    <?php if (!isset($errors['email'])) : ?>
+                        <input type="text" email="email" class="form-control" id="email" value="<?= h($old['email'] ?? $project->email) ?>">
+                    <?php else : ?>
+                        <input type="text" email="email" class="form-control is-invalid" id="email" value="<?= h($old['email'] ?? $project->email) ?>">
+                        <div class="invalid-feedback">
+                            <?php foreach ($errors['email'] as $message) : ?>
+                                <div><?= $message ?></div>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                    <div class="mb-3">
+                    <label for="password" class="form-label">
+                        <span class="text-danger">*</span> パスワード
+                    </label>
+                    <?php if (!isset($errors['password'])) : ?>
+                        <input type="text" password="password" class="form-control" id="password" value="<?= h($old['password'] ?? $project->password) ?>">
+                    <?php else : ?>
+                        <input type="text" password="password" class="form-control is-invalid" id="password" value="<?= h($old['password'] ?? $project->password) ?>">
+                        <div class="invalid-feedback">
+                            <?php foreach ($errors['password'] as $message) : ?>
+                                <div><?= $message ?></div>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                    <button type="button">戻る</button>
+                    <button type="submit">更新</button>
+            </form>
+        </div>
+    </div>
+
+    <?php include HTML_DIR . '/components/footer.php' ?>
+</body>
+
+</html>
